6a5dc7b5780d5c96c379757a1402044aad64e89e
jorritvanvrouwerff <jorrit@grofas.com>
2016-08-18 21:32:37 +0200

    update for water capacity filter

--- src/Modules/Custom/KabolaProductsModule/Services/FilterAdapter.php
+++ src/Modules/Custom/KabolaProductsModule/Services/FilterAdapter.php
@@ -10,221 +10,229 @@ namespace Serff\Cms\Modules\Custom\KabolaProductsModule\Services;
 class FilterAdapter
 {
 
-    /**
-     * @param array $product_options
-     *
-     * @return array
-     */
-    public static function transformProductOptions(array $product_options)
-    {
-        if ((count($product_options) == 0) || (count($product_options) >= 2)) {
-            return ['CV', 'BOILER', 'COMBI'];
-        }
+	/**
+	 * @param array $product_options
+	 *
+	 * @return array
+	 */
+	public static function transformProductOptions(array $product_options)
+	{
+		if ((count($product_options) == 0) || (count($product_options) >= 2)) {
+			return ['CV', 'BOILER', 'COMBI'];
+		}
 
-        if (array_get($product_options, 0) == 'thermostat') {
-            return ['CV', 'COMBI'];
-        }
+		if (array_get($product_options, 0) == 'thermostat') {
+			return ['CV', 'COMBI'];
+		}
 
-        if (array_get($product_options, 0) == 'kettel') {
-            return ['BOILER', 'COMBI'];
-        }
+		if (array_get($product_options, 0) == 'kettel') {
+			return ['BOILER', 'COMBI'];
+		}
 
-        return [];
-    }
+		return [];
+	}
 
-    public static function transformApplicationToAppliance(array $data)
-    {
-        $options = [];
-        foreach ($data as $value) {
-            switch ($value) {
-                case 'centralheating':
-                    $options[] = 'a';
-                    break;
-                case 'hotwater':
-                    $options[] = 'b';
-                    break;
-            }
-        }
+	public static function transformApplicationToAppliance(array $data)
+	{
+		$options = [];
+		foreach ($data as $value) {
+			switch ($value) {
+				case 'centralheating':
+					$options[] = 'a';
+					break;
+				case 'hotwater':
+					$options[] = 'b';
+					break;
+			}
+		}
 
-        return $options;
-    }
+		return $options;
+	}
 
-    /**
-     * @param array $appliance
-     *
-     * @return array
-     */
-    public static function transformAppliciance(array $appliance)
-    {
-        if ((count($appliance) == 0)) {
-            return ['CV', 'BOILER', 'COMBI'];
-        }
+	/**
+	 * @param array $appliance
+	 *
+	 * @return array
+	 */
+	public static function transformAppliciance(array $appliance)
+	{
+		if ((count($appliance) == 0)) {
+			return ['CV', 'BOILER', 'COMBI'];
+		}
 
-        if ((count($appliance) == 2)) {
-            return ['COMBI'];
-        }
+		if ((count($appliance) == 2)) {
+			return ['COMBI'];
+		}
 
-        $types = ['COMBI'];
+		$types = ['COMBI'];
 
-        foreach ($appliance as $item) {
-            if ($item == 'a') {
-                $types[] = 'CV';
-            } elseif ($item == 'b') {
-                $types[] = 'BOILER';
-            }
-        }
+		foreach ($appliance as $item) {
+			if ($item == 'a') {
+				$types[] = 'CV';
+			} elseif ($item == 'b') {
+				$types[] = 'BOILER';
+			}
+		}
 
-        return $types;
-    }
+		return $types;
+	}
 
-    /**
-     * @param $water_cb
-     *
-     * @return array
-     */
-    public static function transformWaterCb($water_cb)
-    {
-        if ((count($water_cb) == 0) || (count($water_cb) >= 4)) {
-            return ['min' => 0, 'max' => 999];
-        }
-        $min = 999;
-        $max = 0;
+	/**
+	 * @param $water_cb
+	 *
+	 * @return array
+	 */
+	public static function transformWaterCb($water_cb)
+	{
+		if ((count($water_cb) == 0) || (count($water_cb) >= 6)) {
+			return ['min' => 0, 'max' => 999];
+		}
+		$min = 999;
+		$max = 0;
 
-        foreach ($water_cb as $item) {
-            switch ($item) {
-                case 'a':
-                    $min = array_get(self::getMinMax($min, $max, 8, 10), 'min');
-                    $max = array_get(self::getMinMax($min, $max, 8, 10), 'max');
-                    break;
-                case 'b':
-                    $min = array_get(self::getMinMax($min, $max, 11, 15), 'min');
-                    $max = array_get(self::getMinMax($min, $max, 11, 15), 'max');
-                    break;
-                case 'c':
-                    $min = array_get(self::getMinMax($min, $max, 16, 20), 'min');
-                    $max = array_get(self::getMinMax($min, $max, 16, 20), 'max');
-                    break;
-                case 'd':
-                    $min = array_get(self::getMinMax($min, $max, 20, 9999), 'min');
-                    $max = array_get(self::getMinMax($min, $max, 20, 9999), 'max');
-                    break;
-            }
-        }
+		foreach ($water_cb as $item) {
+			switch ($item) {
+				case 'a':
+					$min = array_get(self::getMinMax($min, $max, 8, 10), 'min');
+					$max = array_get(self::getMinMax($min, $max, 8, 10), 'max');
+					break;
+				case 'b':
+					$min = array_get(self::getMinMax($min, $max, 11, 15), 'min');
+					$max = array_get(self::getMinMax($min, $max, 11, 15), 'max');
+					break;
+				case 'c':
+					$min = array_get(self::getMinMax($min, $max, 16, 20), 'min');
+					$max = array_get(self::getMinMax($min, $max, 16, 20), 'max');
+					break;
+				case 'd':
+					$min = array_get(self::getMinMax($min, $max, 20, 40), 'min');
+					$max = array_get(self::getMinMax($min, $max, 20, 40), 'max');
+					break;
+				case 'e':
+					$min = array_get(self::getMinMax($min, $max, 40, 60), 'min');
+					$max = array_get(self::getMinMax($min, $max, 40, 60), 'max');
+					break;
+				case 'f':
+					$min = array_get(self::getMinMax($min, $max, 60, 9999), 'min');
+					$max = array_get(self::getMinMax($min, $max, 60, 9999), 'max');
+					break;
+			}
+		}
 
-        return [
-            'min' => $min,
-            'max' => $max,
-        ];
-    }
+		return [
+			'min' => $min,
+			'max' => $max,
+		];
+	}
 
-    /**
-     * @param $min
-     * @param $max
-     * @param $min_val
-     * @param $max_val
-     *
-     * @return array
-     */
-    public static function getMinMax($min, $max, $min_val, $max_val)
-    {
-        if ($min > $min_val) {
-            $min = $min_val;
-        }
-        if ($max < $max_val) {
-            $max = $max_val;
-        }
+	/**
+	 * @param $min
+	 * @param $max
+	 * @param $min_val
+	 * @param $max_val
+	 *
+	 * @return array
+	 */
+	public static function getMinMax($min, $max, $min_val, $max_val)
+	{
+		if ($min > $min_val) {
+			$min = $min_val;
+		}
+		if ($max < $max_val) {
+			$max = $max_val;
+		}
 
-        return [
-            'min' => $min,
-            'max' => $max,
-        ];
-    }
+		return [
+			'min' => $min,
+			'max' => $max,
+		];
+	}
 
-    /**
-     * @param $value
-     *
-     * @return string
-     */
-    public static function transformCapacityValueToSelectItem($value)
-    {
-        if ($value < 10) {
-            return 'a';
-        }
-        if ($value < 19) {
-            return 'b';
-        }
-        if ($value < 29) {
-            return 'c';
-        }
+	/**
+	 * @param $value
+	 *
+	 * @return string
+	 */
+	public static function transformCapacityValueToSelectItem($value)
+	{
+		if ($value < 10) {
+			return 'a';
+		}
+		if ($value < 19) {
+			return 'b';
+		}
+		if ($value < 29) {
+			return 'c';
+		}
 
-        return 'd';
-    }
+		return 'd';
+	}
 
-    /**
-     * @param $capacity
-     *
-     * @return array
-     */
-    public static function transformCapacity($capacity)
-    {
-        if ((count($capacity) == 0) || (count($capacity) >= 4)) {
-            return ['min' => 0, 'max' => 999];
-        }
-        $min = 999;
-        $max = 0;
+	/**
+	 * @param $capacity
+	 *
+	 * @return array
+	 */
+	public static function transformCapacity($capacity)
+	{
+		if ((count($capacity) == 0) || (count($capacity) >= 4)) {
+			return ['min' => 0, 'max' => 999];
+		}
+		$min = 999;
+		$max = 0;
 
-        foreach ($capacity as $item) {
-            switch ($item) {
-                case 'a':
-                    $min = array_get(self::getMinMax($min, $max, 4, 10), 'min');
-                    $max = array_get(self::getMinMax($min, $max, 4, 10), 'max');
-                    break;
-                case 'b':
-                    $min = array_get(self::getMinMax($min, $max, 11, 19), 'min');
-                    $max = array_get(self::getMinMax($min, $max, 11, 19), 'max');
-                    break;
-                case 'c':
-                    $min = array_get(self::getMinMax($min, $max, 20, 29), 'min');
-                    $max = array_get(self::getMinMax($min, $max, 20, 29), 'max');
-                    break;
-                case 'd':
-                    $min = array_get(self::getMinMax($min, $max, 30, 116), 'min');
-                    $max = array_get(self::getMinMax($min, $max, 30, 116), 'max');
-                    break;
-            }
-        }
+		foreach ($capacity as $item) {
+			switch ($item) {
+				case 'a':
+					$min = array_get(self::getMinMax($min, $max, 4, 10), 'min');
+					$max = array_get(self::getMinMax($min, $max, 4, 10), 'max');
+					break;
+				case 'b':
+					$min = array_get(self::getMinMax($min, $max, 11, 19), 'min');
+					$max = array_get(self::getMinMax($min, $max, 11, 19), 'max');
+					break;
+				case 'c':
+					$min = array_get(self::getMinMax($min, $max, 20, 29), 'min');
+					$max = array_get(self::getMinMax($min, $max, 20, 29), 'max');
+					break;
+				case 'd':
+					$min = array_get(self::getMinMax($min, $max, 30, 116), 'min');
+					$max = array_get(self::getMinMax($min, $max, 30, 116), 'max');
+					break;
+			}
+		}
 
-        return [
-            'min' => $min,
-            'max' => $max,
-        ];
-    }
+		return [
+			'min' => $min,
+			'max' => $max,
+		];
+	}
 
-    /**
-     * @param $efficiency
-     *
-     * @return array
-     */
-    public static function transformEfficiency($efficiency)
-    {
-        if ((count($efficiency) == 0) || (count($efficiency) >= 2)) {
-            return [90, 94];
-        }
-        $items = [];
+	/**
+	 * @param $efficiency
+	 *
+	 * @return array
+	 */
+	public static function transformEfficiency($efficiency)
+	{
+		if ((count($efficiency) == 0) || (count($efficiency) >= 2)) {
+			return [90, 94];
+		}
+		$items = [];
 
-        foreach ($efficiency as $item) {
-            switch ($item) {
-                case 'a':
-                    $items[] = 90;
-                    break;
-                case 'b':
-                default:
-                    $items[] = 94;
-                    break;
-            }
-        }
+		foreach ($efficiency as $item) {
+			switch ($item) {
+				case 'a':
+					$items[] = 90;
+					break;
+				case 'b':
+				default:
+					$items[] = 94;
+					break;
+			}
+		}
 
-        return $items;
-    }
+		return $items;
+	}
 
 }
