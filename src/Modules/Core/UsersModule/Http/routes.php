7672a78f939cdbd6bcd13ac07ce7f6c9080a2b06
jorritvanvrouwerff <info@serff.nl>
2017-03-02 15:41:52 +0100

    typo

--- src/Modules/Core/UsersModule/Http/routes.php
+++ src/Modules/Core/UsersModule/Http/routes.php
@@ -1,3 +1,3 @@
 <?php
 
-Route::get('admin/login', ['as' => 'admin.login', 'uses' => 'App\Http\Controllers\Auth\LoginController@showLoginForm']);
+Route::get('admin/login', ['as' => 'admin.login', 'uses' => '\App\Http\Controllers\Auth\LoginController@showLoginForm']);
