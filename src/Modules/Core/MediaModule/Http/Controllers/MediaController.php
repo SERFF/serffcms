3272c60685a31a0122ddc8cfc2d82c5feadb0a2c
jorritvanvrouwerff <info@serff.nl>
2017-06-19 10:43:40 +0200

    fix for image cache

--- src/Modules/Core/MediaModule/Http/Controllers/MediaController.php
+++ src/Modules/Core/MediaModule/Http/Controllers/MediaController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Serff\Cms\Modules\Core\MediaModule\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
 use Serff\Cms\Core\Controllers\Controller;
 use Serff\Cms\Modules\Core\MediaModule\Domain\Models\Media;
 use Serff\Cms\Modules\Core\MediaModule\Services\MediaService;
@@ -19,10 +20,14 @@ class MediaController extends Controller
 
     public function getImage($id, $name, $width = null, $height = null)
     {
-        $item = $this->mediaService->getImage(Media::find($id), $width, $height);
-        
-        $image = \Image::make(array_get($item, 'file'));
-        
-        return $image->response();
+        $cache_key = sprintf('image_cache_%s', md5($id.$width.$height.$name));
+
+        return Cache::remember($cache_key, 99999, function () use ($id, $width, $height) {
+            $item = $this->mediaService->getImage(Media::find($id), $width, $height);
+
+            $image = \Image::make(array_get($item, 'file'));
+
+            return $image->response();
+        });
     }
 }
